9366c129c8c4b65730487fbf5c7f260f9352aa51
Hallur Kristinn Hallsson <hallurkrist@gmail.com>
2021-04-07 14:45:02 +0000

    initial shiftlist view

---
+++ app/src/main/java/is/hi/finnbogi_mobile/ShiftListActivity.java
@@ -0,0 +1,14 @@
+package is.hi.finnbogi_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShiftListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shift_list);
+    }
+}
